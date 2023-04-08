d23a1dc33f55e0bc45be317e614449908e8c4b11
quervolvh <quervolvh@gmil.com>
2023-04-08 16:29:09 +0100

    big video

--- public/youtubeScript.js
+++ public/youtubeScript.js
@@ -36,12 +36,6 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
 
-  // if (event.data == YT.PlayerState.PLAYING && !done) {
-  //     setTimeout(stopVideo, 6000);
-  //     done = true;
-  //   }
-
-  console.log("mmmmeee", event)
-
   player.playVideo();
+
 }
