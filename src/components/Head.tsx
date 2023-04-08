4a8df3773b01848871cb7bd21a16979fd56ed73b
quervolvh <quervolvh@gmil.com>
2023-04-08 14:40:31 +0100

    player

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
-export const HtmlHead:React.FC<{ title: any }> = ({ title }) => {
+export const HtmlHead: React.FC<{ title: any }> = ({ title }) => {
   const description = '';
   const link = '';
   const logo = '';
@@ -23,40 +23,48 @@ export const HtmlHead:React.FC<{ title: any }> = ({ title }) => {
   }`;
 
   return (
-    <Head>
-      <title> {title ? title : ""} </title>
-      <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
-      <link href="/fonts/style.css" rel="stylesheet" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
-      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+    <>
+      <Head>
+        <title> {title ? title : ""} </title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+        <link href="/fonts/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={link} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={logo} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={link} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={logo} />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={link} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={logo} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={link} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={logo} />
 
-      <script>
-        {process.env.NEXT_PUBLIC_NODE_ENV !== 'development' && (window as any)?.__REACT_DEVTOOLS_GLOBAL_HOOK__
-          ? 'window?.__REACT_DEVTOOLS_GLOBAL_HOOK__?.inject = function(){}'
-          : ''}
-      </script>
+        <script>
+          {process.env.NEXT_PUBLIC_NODE_ENV !== 'development' && (window as any)?.__REACT_DEVTOOLS_GLOBAL_HOOK__
+            ? 'window?.__REACT_DEVTOOLS_GLOBAL_HOOK__?.inject = function(){}'
+            : ''}
+        </script>
 
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: seoAttributes }} />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: seoAttributes }} />
 
-      <link
-        rel="stylesheet"
-        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-        crossOrigin="anonymous"
-      />
-    </Head>
+      <script src="youtubeScript.js"> </script>
+
+
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+
+
+    </>
   );
 };
