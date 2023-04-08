be90dfbe03e8cf0c5c73a5b9370af95fd61d2828
quervolvh <quervolvh@gmil.com>
2023-04-08 14:43:18 +0100

    player

--- src/common/Studio/StudioCaption.tsx
+++ src/common/Studio/StudioCaption.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ComponentHolder } from 'components';
 import { Header } from 'layout/LandingLayout/header';
 import { classnames } from 'utils';
