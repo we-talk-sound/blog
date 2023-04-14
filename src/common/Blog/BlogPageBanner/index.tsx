6928d53b0294b52e225c66a8083800c7c7110f2a
quervolvh <quervolvh@gmil.com>
2023-04-14 21:17:29 +0100

    --- no base ---

--- src/common/Blog/BlogPageBanner/index.tsx
+++ src/common/Blog/BlogPageBanner/index.tsx
@@ -7,13 +7,13 @@ import { BlogBanner } from 'common/PageZero/BlogBanner';
 import { transformStory } from 'utils/blog';
 import { blogItemType } from 'types';
 
-export const BlogPageBanner: React.FC<Props> = ({ isMobile, deviceWidth , story , dataSource , dataSourceLoader }) => {
+export const BlogPageBanner: React.FC<Props> = ({ isMobile, deviceWidth, story, dataSource, dataSourceLoader }) => {
 
     const router = useRouter();
 
-    const { slug , category } = router.query;
+    const { slug, category } = router.query;
 
-    const story_ = story ? { ...transformStory(story , false) } : undefined;
+    const story_ = story ? { ...transformStory(story, false) } : undefined;
 
     return (
 
@@ -27,16 +27,16 @@ export const BlogPageBanner: React.FC<Props> = ({ isMobile, deviceWidth , story 
 
             <Header withFrame={true} isMobile={isMobile} deviceWidth={deviceWidth} active={"/blog"} />
 
-            {(!slug && !story_) && <BaseBlogHeading category={String(category || "music")} />}
+            {(!slug && !story_) && <BaseBlogHeading category={String(category)} />}
 
-            <BlogBanner 
-            
-                bannerMode={true} 
-                
+            <BlogBanner
+
+                bannerMode={true}
+
                 story={story_}
 
                 slug={slug ? String(slug) : undefined}
-            
+
                 dataSource={dataSource}
 
                 dataSourceLoader={dataSourceLoader}
