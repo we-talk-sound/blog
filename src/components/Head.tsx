c8cd12be9a73b71a55757e0c52adc5953480c386
quervolvh <quervolvh@gmil.com>
2023-05-11 22:36:14 +0100

    lets see

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -25,28 +25,28 @@ export const HtmlHead: React.FC<{ title: any }> = ({ title }) => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={link} />
+        <meta property="og:type" content="website" > </meta>
+        <meta property="og:url" content={link} > </meta>
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={logo} />
+        <meta property="og:description" content={description} > </meta>
+        <meta property="og:image" content={logo} > </meta>
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={link} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={logo} />
+        <meta property="twitter:card" content="summary_large_image" > </meta>
+        <meta property="twitter:url" content={link} > </meta>
+        <meta property="twitter:title" content={title} > </meta>
+        <meta property="twitter:description" content={description} > </meta>
+        <meta property="twitter:image" content={logo} > </meta>
 
       </Head>
 
-      <link href="/fonts/style.css" rel="stylesheet" />
+      <link href="/fonts/style.css" rel="stylesheet" > </link>
 
       <link
         rel="stylesheet"
         href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
         crossOrigin="anonymous"
-      />
+      ></link>
 
 
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: seoAttributes }} />
