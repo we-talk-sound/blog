541ca3f7d550daf6cc5687465d94377556027e58
quervolvh <quervolvh@gmil.com>
2023-05-11 22:40:59 +0100

    meta

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -20,36 +20,42 @@ export const HtmlHead: React.FC<{ title: any }> = ({ title }) => {
   return (
     <>
       <Head>
-        <title> {title ? title : "Wetalksound"} </title>
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <meta property="og:type" content="website" > </meta>
-        <meta property="og:url" content={link} > </meta>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} > </meta>
+        <title> {title ? title : "Wetalksound"} </title>
+        <meta name="description" content={description}> </meta>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
+
+        <meta property="og:type" content="website"> </meta>
+        <meta property="og:url" content={link}> </meta>
+        <meta property="og:title" content={title}> </meta>
+        <meta property="og:description" content={description}> </meta>
         <meta property="og:image" content={logo} > </meta>
 
-        <meta property="twitter:card" content="summary_large_image" > </meta>
-        <meta property="twitter:url" content={link} > </meta>
-        <meta property="twitter:title" content={title} > </meta>
-        <meta property="twitter:description" content={description} > </meta>
-        <meta property="twitter:image" content={logo} > </meta>
+        <meta property="twitter:card" content="summary_large_image"> </meta>
+        <meta property="twitter:url" content={link}> </meta>
+        <meta property="twitter:title" content={title}> </meta>
+        <meta property="twitter:description" content={description}> </meta>
+        <meta property="twitter:image" content={logo}> </meta>
+
+        <link href="/fonts/style.css" rel="stylesheet" /> 
+
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossOrigin="anonymous"
+        />
+
+        <script>
+          {process.env.NEXT_PUBLIC_NODE_ENV !== 'development'
+            ? 'window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function(){}'
+            : ''}
+        </script>
+
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: seoAttributes }} />
 
       </Head>
-
-      <link href="/fonts/style.css" rel="stylesheet" > </link>
-
-      <link
-        rel="stylesheet"
-        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-        crossOrigin="anonymous"
-      ></link>
-
-
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: seoAttributes }} />
 
     </>
   );
