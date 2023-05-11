af83dc73d8b1c750e341686cdd76a0477294a595
quervolvh <quervolvh@gmil.com>
2023-05-11 22:47:48 +0100

    no link

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,46 +3,38 @@ import Head from 'next/head';
 
 export const HtmlHead: React.FC<{ title: any }> = ({ title }) => {
   const description = "We are a  360  creative company leveraging technology, community & content to design delightful experiences.";
-  const link = 'http://wetalksound.co';
 
   return (
-    <>
-      <Head>
+    <Head>
 
-        <title> {title ? title : "Wetalksound"} </title>
-        <meta name="description" content={description}> </meta>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
+      <title> {title ? title : "Wetalksound"} </title>
+      <meta name="description" content={description}> </meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
 
-        <meta property="og:type" content="website"> </meta>
-        <meta property="og:url" content={link}> </meta>
-        <meta property="og:title" content={title}> </meta>
-        <meta property="og:description" content={description}> </meta>
+      <meta property="og:type" content="website"> </meta>
+      <meta property="og:title" content={title}> </meta>
+      <meta property="og:description" content={description}> </meta>
 
 
-        <meta property="twitter:card" content="summary_large_image"> </meta>
-        <meta property="twitter:url" content={link}> </meta>
-        <meta property="twitter:title" content={title}> </meta>
-        <meta property="twitter:description" content={description}> </meta>
+      <meta property="twitter:title" content={title}> </meta>
+      <meta property="twitter:description" content={description}> </meta>
 
 
-        <link href="/fonts/style.css" rel="stylesheet" /> 
+      <link href="/fonts/style.css" rel="stylesheet" />
 
-        <link
-          rel="stylesheet"
-          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-          crossOrigin="anonymous"
-        />
+      <link
+        rel="stylesheet"
+        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+        crossOrigin="anonymous"
+      />
 
-        <script>
-          {process.env.NEXT_PUBLIC_NODE_ENV !== 'development'
-            ? 'window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function(){}'
-            : ''}
-        </script>
+      <script>
+        {process.env.NEXT_PUBLIC_NODE_ENV !== 'development'
+          ? 'window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function(){}'
+          : ''}
+      </script>
 
-      </Head>
-
-    </>
+    </Head>
   );
 };
