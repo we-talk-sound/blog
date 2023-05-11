34a2b7815615067e2ac9aa631ce04c6f46f35887
quervolvh <quervolvh@gmil.com>
2023-05-11 22:58:58 +0100

    __-

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import Head from 'next/head';
 
-export const HtmlHead: React.FC<{ title: any }> = ({ title }) => {
-  
-  const description = "We are a  360  creative company leveraging technology, community & content to design delightful experiences.";
+export const HtmlHead: React.FC<{ title?: any }> = ({ }) => {
+
+  // const description = "We are a  360  creative company leveraging technology, community & content to design delightful experiences.";
 
   return (
     <Head>
 
       <title> Wetalksound </title>
-      <meta name="description" content={description}> </meta>
+      <meta
+
+        name="description"
+
+        content={"We are a  360  creative company leveraging technology, community & content to design delightful experiences."}>
+
+      </meta>
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
 
       <meta property="og:type" content="website"> </meta>
-      <meta property="og:title" content={title}> </meta>
-      <meta property="og:description" content={description}> </meta>
+      <meta property="og:title" content={"Wetalksound"}> </meta>
+
+      <meta
+
+        property="og:description"
+
+        content={"We are a  360  creative company leveraging technology, community & content to design delightful experiences."}>
+
+      </meta>
 
 
-      <meta property="twitter:title" content={title}> </meta>
-      <meta property="twitter:description" content={description}> </meta>
+      <meta property="twitter:title" content={"Wetalksound"}> </meta>
+
+      <meta
+
+        property="twitter:description"
+
+        content={"We are a  360  creative company leveraging technology, community & content to design delightful experiences."}>
+
+      </meta>
 
 
       <link href="/fonts/style.css" rel="stylesheet" />
