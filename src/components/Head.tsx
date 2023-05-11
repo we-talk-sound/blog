7d1beb1b700a7d307882847dc595a0be10834364
quervolvh <quervolvh@gmil.com>
2023-05-11 23:44:39 +0100

    metaaa

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,36 +1,40 @@
 import Head from 'next/head';
 import React from 'react';
 
-const HtmlHead: React.FC<{ title?: any }> = ({ }) => {
+const HtmlHead: React.FC<{ title?: string }> = ({ }) => {
 
   return (
 
     <Head>
+      <title> {"Wetalksound"} </title>
 
-      <title> Wetalksound </title>
+      <link rel="icon" href="/favicon.ico" />
 
-      <meta
+      <title>We are powering Africa's creative ecosystem with technology and capital.</title>
+      <meta name="title" content="We are powering Africa's creative ecosystem with technology and capital."></meta>
+      <meta name="description" content="TheAfroGarage | Powering Africa's creative industry with technology & capital"></meta>
 
-        name="description"
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://www.theafrogarage.com/"></meta>
+      <meta property="og:title" content="We are powering Africa's creative ecosystem with technology and capital."></meta>
+      <meta property="og:description" content="TheAfroGarage | Powering Africa's creative industry with technology & capital"></meta>
+      <meta property="og:image" content="https://www.theafrogarage.com/apple-touch-icon.png"></meta>
 
-        content="We are a  360  creative company leveraging technology, community & content to design delightful experiences.">
-
-      </meta>
-
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"> </meta>
-
-      <meta property="og:type" content="website"> </meta>
-      <meta property="og:title" content="Wetalksound"> </meta>
-
-      <meta
-
-        property="og:description"
-
-        content="We are a  360  creative company leveraging technology, community & content to design delightful experiences.">
-
-      </meta>
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:url" content="https://www.theafrogarage.com/"></meta>
+      <meta property="twitter:title" content="We are powering Africa's creative ecosystem with technology and capital."></meta>
+      <meta property="twitter:description" content="TheAfroGarage | Powering Africa's creative industry with technology & capital"></meta>
+      <meta property="twitter:image" content="https://www.theafrogarage.com/apple-touch-icon.png"></meta>
 
       <link href="/fonts/style.css" rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1"></meta>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
+
+      <script>
+        {process.env.NEXT_PUBLIC_NODE_ENV !== 'development'
+          ? 'window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function(){}'
+          : ''}
+      </script>
 
       <link
         rel="stylesheet"
@@ -38,7 +42,7 @@ const HtmlHead: React.FC<{ title?: any }> = ({ }) => {
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
         crossOrigin="anonymous"
       />
-
+     
     </Head>
   );
 };
