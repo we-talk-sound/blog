0ed4c0088ada06d78cd2c607e0017ecda4feb0ae
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-18 22:56:46 +0100

    chore: format code

--- src/layout/LandingLayout/Footer/index.tsx
+++ src/layout/LandingLayout/Footer/index.tsx
@@ -4,45 +4,22 @@ import { LandingFooterServices } from 'constants/index';
 import LandingLayoutFooterCr from '../LandingLayoutFooterCr';
 import { classnames } from 'utils';
 
-const LandingLayoutFooter: React.FC<{ footerClass?: string, footerId?: string }> = ({ footerClass, footerId }) => {
-
-    return (
-
-        <div
-
-            {...(footerId ? { id: footerId } : {})}
-
-            className={classnames("landingLayout-footer footer", footerClass)}>
-
-            <div className="landingLayout-footer-holder">
-
-                <div className='landingLayout-footer-holder-tabs'>
-
-                    {LandingFooterServices.map((item, index) =>
-
-                        <div
-
-                            className="landingLayout-footer-tab landingLayout-footer-tab-contact"
-
-                            key={`footer-contact-${index}`}
-
-                        >
-
-                            <ViewFormatter {...item} />
-
-                        </div>
-
-                    )}
-
-                </div>
-
-                <LandingLayoutFooterCr />
-
+const LandingLayoutFooter: React.FC<{ footerClass?: string; footerId?: string }> = ({ footerClass, footerId }) => {
+  return (
+    <div {...(footerId ? { id: footerId } : {})} className={classnames('landingLayout-footer footer', footerClass)}>
+      <div className="landingLayout-footer-holder">
+        <div className="landingLayout-footer-holder-tabs">
+          {LandingFooterServices.map((item, index) => (
+            <div className="landingLayout-footer-tab landingLayout-footer-tab-contact" key={`footer-contact-${index}`}>
+              <ViewFormatter {...item} />
             </div>
-
+          ))}
         </div>
 
-    );
-}
+        <LandingLayoutFooterCr />
+      </div>
+    </div>
+  );
+};
 
 export default LandingLayoutFooter;
