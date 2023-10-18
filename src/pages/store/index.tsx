60b1f9718ed1cc77bb1f5e087be094aaaa54a9c1
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-18 22:46:58 +0100

    style: change background image for store page

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -4,54 +4,32 @@ import { ComponentHolder } from 'components';
 import { Header } from 'layout/LandingLayout/header';
 
 const Store: React.FC<Props> = ({ isMobile, deviceWidth }) => {
+  return (
+    <LandingLayout
+      headTitle={'WETALKSOUND | STORE'}
+      isMobile={isMobile}
+      deviceWidth={deviceWidth}
+      showFooter={true}
+      showHeader={false}
+    >
+      <ComponentHolder
+        className="page-zero no-border page-agency page-store"
+        style={{ backgroundImage: `url(assets/background/jerseys.png)` }}
+        bodyClass="page-agency-caption"
+      >
+        <Header withFrame={true} isMobile={isMobile} deviceWidth={deviceWidth} active={'/store'} />
 
-    return (
-
-        <LandingLayout
-            headTitle={"WETALKSOUND | EVENTS"}
-            isMobile={isMobile}
-            deviceWidth={deviceWidth}
-            showFooter={false}
-            showHeader={false}
-
-        >
-
-            <ComponentHolder
-
-                className='page-zero no-border page-agency page-store'
-
-                style={{ backgroundImage: `url(assets/background/soon.png)` }}
-
-                bodyClass='page-agency-caption'>
-
-                <Header
-
-                    withFrame={true}
-
-                    isMobile={isMobile}
-
-                    deviceWidth={deviceWidth}
-
-                    active={"/store"}
-
-                />
-
-                <div className='page-store-text'>
-
-                    <h1> Coming Soon </h1>
-
-                </div>
-
-            </ComponentHolder >
-
-        </LandingLayout >
-
-    )
-}
+        <div className="page-store-text">
+          <h1> Coming Soon </h1>
+        </div>
+      </ComponentHolder>
+    </LandingLayout>
+  );
+};
 
 export default Store;
 
 interface Props {
-    isMobile: boolean,
-    deviceWidth: number
+  isMobile: boolean;
+  deviceWidth: number;
 }
