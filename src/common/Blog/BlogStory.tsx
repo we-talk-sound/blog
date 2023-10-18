e975370aa851988dab57ab07620f8ea110742555
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-18 22:41:56 +0100

    feat: add share buttons to footer of blog content

--- src/common/Blog/BlogStory.tsx
+++ src/common/Blog/BlogStory.tsx
@@ -1,30 +1,47 @@
 import { ComponentHolder } from 'components';
 import React from 'react';
 import { blogItemType } from 'types';
+import { transformStory } from 'utils/blog';
+import BlogCardGrid from './BlogCardGrid';
 
 export const BlogStory: React.FC<Props> = ({ story }) => {
+  const story_ = story ? { ...transformStory(story, false) } : undefined;
 
-    return (
+  const SOCIALS = [
+    { key: 'linkedin', svg: '/svg/linkedin.svg' },
+    { key: 'instagram', svg: '/svg/instagram.svg' },
+    { key: 'twitter', svg: '/svg/twitter.svg' }
+  ];
 
-        <ComponentHolder
+  const handleShare = (key: string) => {
+    alert('Sharing on ' + key);
+  };
 
-            className='no-border page-events-expanded-body'
+  return (
+    <ComponentHolder className="no-border page-events-expanded-body" bodyClass="page-zero-content">
+      <div className="page-events-expanded-body-content">
+        {story?.content?.rendered && <div dangerouslySetInnerHTML={{ __html: story?.content?.rendered }} />}
+      </div>
 
-            bodyClass='page-zero-content'>
+      <div className="page-events-expanded-body-footer">
+        <div className="page-events-expanded-body-footer-author">
+          About Author: <span>{story_?.author}</span>
+        </div>
+        <div className="page-events-expanded-body-footer-share">
+          <span>Share this Article: </span>
+          {SOCIALS.map(social => (
+            <button type="button" key={social.key} onClick={() => handleShare(social.key)}>
+              <img src={social.svg} />
+            </button>
+          ))}
+        </div>
+      </div>
 
-            <div className='page-events-expanded-body-content'>
-
-                {story?.content?.rendered && <div dangerouslySetInnerHTML={{ __html: story?.content?.rendered }} />}
-
-            </div>
-
-        </ComponentHolder>
-
-    )
-}
+      <BlogCardGrid title={'Related Post'} showAction={false} items={[...Array(4)]} />
+    </ComponentHolder>
+  );
+};
 
 interface Props {
-
-    story: blogItemType
-
+  story: blogItemType;
 }
