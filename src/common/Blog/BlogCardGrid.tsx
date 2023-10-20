e7569424ea3a1fc6ba7b75e7a4bdabdf0c1e6aa6
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-20 11:19:09 +0100

    style: fix responsiveness

--- src/common/Blog/BlogCardGrid.tsx
+++ src/common/Blog/BlogCardGrid.tsx
@@ -13,7 +13,6 @@ const BlogCardGrid = ({ title, action, variant, items = [], actionText = 'See Mo
             href="/blog/music/american-sierra-leonean-artist-laik-returns-to-the-scene-with-new-single-ahje"
             className="page-blog-blogcard-item"
           >
-            {/* <div > */}
             <img />
             <div className="page-blog-blogcard-item-content">
               {variant !== 'videos' && (
@@ -26,14 +25,11 @@ const BlogCardGrid = ({ title, action, variant, items = [], actionText = 'See Mo
                 </>
               )}
               <h4 className="title">Rave And Roses Twitter Analysis and a couple of others</h4>
-              {variant === 'topreads' && (
-                <p className="description">
-                  Showing no signs of slowing down, Lojay’s new single, “Leader!” adds yet another string to his bow.
-                  Showing no signs of slowing down, Lojay’s new single, “Leader!” adds yet another string to his bow.
-                </p>
-              )}
+              <p className="description">
+                Showing no signs of slowing down, Lojay’s new single, “Leader!” adds yet another string to his bow.
+                Showing no signs of slowing down, Lojay’s new single, “Leader!” adds yet another string to his bow.
+              </p>
             </div>
-            {/* </div> */}
           </Link>
         ))}
       </div>
