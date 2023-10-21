62d36e1f043ee90609ded794f4fc7c95a1015663
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-21 20:35:05 +0100

    chore: use category from API

--- src/pages/blog/[category]/index.tsx
+++ src/pages/blog/[category]/index.tsx
@@ -6,10 +6,8 @@ import { NewsLetter } from 'common/NewsLetter';
 import { ComponentHolder } from 'components';
 import BlogCardGrid from 'common/Blog/BlogCardGrid';
 import { blogItemType } from 'types';
-import { baseSlugPairs } from 'redux/reducers/blogReducer';
 
 const BlogCategoryPage: React.FC<Props> = ({ isMobile, deviceWidth, category, posts }) => {
-
   return (
     <LandingLayout
       headTitle={`WETALKSOUND`}
@@ -20,12 +18,7 @@ const BlogCategoryPage: React.FC<Props> = ({ isMobile, deviceWidth, category, po
       showFooter={true}
       showHeader={false}
     >
-      <BlogPageBanner
-        isMobile={isMobile}
-        deviceWidth={deviceWidth}
-        items={posts.slice(0, 6)}
-        category={category}
-      />
+      <BlogPageBanner isMobile={isMobile} deviceWidth={deviceWidth} items={posts.slice(0, 6)} category={category} />
 
       <ComponentHolder className="no-border page-events-expanded-body" bodyClass="page-zero-content">
         <div className="page-events-expanded-body-content">
@@ -41,13 +34,23 @@ const BlogCategoryPage: React.FC<Props> = ({ isMobile, deviceWidth, category, po
 
 export async function getServerSideProps({ params }: { params: { category: string } }) {
   const { category } = params;
-  const categoryID = (baseSlugPairs as any)[category]?.id;
-  console.log({ categoryID });
+  let activeCategory = await fetch(`https://blog-admin.wetalksound.co/wp-json/wp/v2/categories?slug=${category}`);
+  activeCategory = await activeCategory.json();
+
+  // redirect to blog page if no category
+  if (!activeCategory) {
+    return {
+      redirect: {
+        destination: '/blog',
+        permanent: false
+      }
+    };
+  }
 
   const baseUrl =
     'https://blog-admin.wetalksound.co/wp-json/wp/v2/posts?_embed=1&_fields=title,slug,categories,date,_links,yoast_head_json.description';
-
-  let data = await fetch(`${baseUrl}&categories=${categoryID}&per_page=18`);
+  // @ts-ignore
+  let data = await fetch(`${baseUrl}&categories=${activeCategory[0].id}&per_page=18`);
   data = await data.json();
 
   return {
@@ -63,35 +66,3 @@ interface Props {
   posts: blogItemType[];
   category: string;
 }
-
-// export async function getServerSideProps({ params }: { params: { category: string } }) {
-//   //   let data: any = null;
-//   const { category } = params;
-
-//   //   const url = `https://blog-admin.wetalksound.co/wp-json/wp/v2/posts?slug=${slug}&_embed`;
-//   //   const res = await fetch(url);
-//   //   data = await res.json();
-//   //   data = Array.isArray(data) ? data[0] : data;
-
-//   // redirect to blog page if no data
-//   //   if (!data) {
-//   //     return {
-//   //       redirect: {
-//   //         destination: '/blog2',
-//   //         permanent: false
-//   //       }
-//   //     };
-//   //   }
-
-//   return {
-//     props: { category }
-//   };
-// }
-
-// interface Props {
-//   isMobile: boolean;
-//   deviceWidth: number;
-//   category: string;
-// }
-
-// export default BlogCategoryPage;
