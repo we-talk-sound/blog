395b4d803702b1335ae540798eb49bb791baa8dd
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-22 13:47:42 +0100

    fix: fix up categories lists

--- src/redux/reducers/blogReducer.tsx
+++ src/redux/reducers/blogReducer.tsx
@@ -3,246 +3,246 @@ import { blogCategoryItemType, blogEntryTypes, blogPageData, storeBlogEntry } fr
 import { shallowCompare } from 'utils';
 
 const base = {
+  data: [],
+  page: 1,
+  perPage: 20,
+  pageCount: 1,
+  loader: false,
+  queryParam: {},
+  error: false
+};
+
+const baseCategoryPairs = {
+  '1': {
+    id: 1,
+    description: '',
+    name: 'Uncategorized',
+    slug: 'uncategorized'
+  },
+  '2': {
+    id: 2,
+    description: 'Community. Because we are stronger together than on our own. Articles about community-building.',
+    name: 'Community',
+    slug: 'community'
+  },
+  '3': {
+    id: 3,
+    description: 'We talk to all sorts of interesting guests and unearth their wisdom.',
+    name: 'Conversations',
+    slug: 'conversations'
+  },
+  '4': {
+    id: 4,
+    description:
+      // eslint-disable-next-line max-len
+      "From networking opportunities and industry workshops to concerts and shows by your favourite artistes and brands, we've got something for everyone.",
+    name: 'Events',
+    slug: 'events'
+  },
+  '5': {
+    id: 5,
+    description: 'Data deep dives and introspective thought pieces, we take a look behind the curtain.',
+    name: 'Insights',
+    slug: 'insights'
+  },
+  '6': {
+    id: 6,
+    description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
+    name: 'Music',
+    slug: 'music'
+  },
+  '7': {
+    id: 7,
+    description:
+      // eslint-disable-next-line max-len
+      'We pride ourselves on discovering and nurturing talented artists from a wide range of musical genres. From Afrobeats and HipHop, to Alte &amp; other niche sounds.',
+    name: 'Record Label',
+    slug: 'labels'
+  }
+};
+
+const baseSlugPairs = {
+    'uncategorized': {
+        id: 1,
+        description: '',
+        name: 'Uncategorized',
+        slug: 'uncategorized'
+      },
+      'community': {
+        id: 2,
+        description: 'Community. Because we are stronger together than on our own. Articles about community-building.',
+        name: 'Community',
+        slug: 'community'
+      },
+      'conversations': {
+        id: 3,
+        description: 'We talk to all sorts of interesting guests and unearth their wisdom.',
+        name: 'Conversations',
+        slug: 'conversations'
+      },
+      'events': {
+        id: 4,
+        description:
+          // eslint-disable-next-line max-len
+          "From networking opportunities and industry workshops to concerts and shows by your favourite artistes and brands, we've got something for everyone.",
+        name: 'Events',
+        slug: 'events'
+      },
+      'insights': {
+        id: 5,
+        description: 'Data deep dives and introspective thought pieces, we take a look behind the curtain.',
+        name: 'Insights',
+        slug: 'insights'
+      },
+      'music': {
+        id: 6,
+        description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
+        name: 'Music',
+        slug: 'music'
+      },
+      'labels': {
+        id: 7,
+        description:
+          // eslint-disable-next-line max-len
+          'We pride ourselves on discovering and nurturing talented artists from a wide range of musical genres. From Afrobeats and HipHop, to Alte &amp; other niche sounds.',
+        name: 'Record Label',
+        slug: 'labels'
+      }
+};
+
+const initialState: storeBlogEntry = {
+  dashboardBlogs: base,
+
+  blogCategoryStories: { ...base, categoryData: {} },
+
+  categories: {
     data: [],
+    pairs: baseCategoryPairs,
+    slugPairs: baseSlugPairs,
     page: 1,
     perPage: 20,
     pageCount: 1,
     loader: false,
     queryParam: {},
     error: false
-};
+  },
 
-const baseCategoryPairs = {
-    '1': {
-        id: 1,
-        description: 'Community. Because we are stronger together than on our own. Articles about community-building.',
-        name: 'Community',
-        slug: 'community',
-    },
-    '2': {
-        id: 2,
-        description: 'Data deep dives and introspective thought pieces, we take a look behind the curtain.',
-        name: 'Insights',
-        slug: 'insights',
-    },
-    '3': {
-        id: 3,
-        description: 'We talk to all sorts of interesting guests and unearth their wisdom.',
-        name: 'Conversations',
-        slug: 'conversations',
-    },
-    '4': {
-        id: 4,
-        description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
-        name: 'Music',
-        slug: 'music',
-    },
-    '5': {
-        id: 5,
-        description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
-        name: 'All',
-        slug: 'all',
-    },
-};
-
-const baseSlugPairs = {
-    'community': {
-        id: 1,
-        description: 'Community. Because we are stronger together than on our own. Articles about community-building.',
-        name: 'Community',
-        slug: 'community',
-    },
-    'insights': {
-        id: 2,
-        description: 'Data deep dives and introspective thought pieces, we take a look behind the curtain.',
-        name: 'Insights',
-        slug: 'insights',
-    },
-    'conversations': {
-        id: 3,
-        description: 'We talk to all sorts of interesting guests and unearth their wisdom.',
-        name: 'Conversations',
-        slug: 'conversations',
-    },
-    'music': {
-        id: 4,
-        description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
-        name: 'Music',
-        slug: 'music',
-    },
-    'all': {
-        id: 5,
-        description: 'Releases, Reviews and More. Experience the best music from our community and beyond.',
-        name: 'Music',
-        slug: 'music',
-    }
-};
-
-const initialState: storeBlogEntry = {
-
-    dashboardBlogs: base,
-
-    blogCategoryStories: { ...base, categoryData: {} },
-
-    categories: {
-        data: [],
-        pairs: baseCategoryPairs,
-        slugPairs: baseSlugPairs,
-        page: 1,
-        perPage: 20,
-        pageCount: 1,
-        loader: false,
-        queryParam: {},
-        error: false
-    },
-
-    blogSingleStories: {}
+  blogSingleStories: {}
 };
 
 const createCategoryPairs = (categories: blogCategoryItemType[], useSlug?: boolean) => {
+  const categoryPairs: { [key: string | number]: blogCategoryItemType } = {};
 
-    const categoryPairs: { [key: string | number]: blogCategoryItemType } = {};
+  categories.forEach(item => (categoryPairs[useSlug ? item.slug : item.id] = item));
 
-    categories.forEach(item => (categoryPairs[useSlug ? item.slug : item.id] = item));
-
-    return categoryPairs;
-
-}
+  return categoryPairs;
+};
 
 const dataToStore = (
+  state: { categoryData: { [key: string]: blogPageData } },
 
-    state: { categoryData: { [key: string]: blogPageData } },
+  data: Array<blogEntryTypes>,
 
-    data: Array<blogEntryTypes>,
+  ref: blogEntryTypes,
 
-    ref: blogEntryTypes,
-
-    params: { page?: number, category?: string }
-
+  params: { page?: number; category?: string }
 ) => {
+  if (ref === 'blogCategoryStories' && params?.category) {
+    return {
+      categoryData: state?.categoryData?.[params?.category]
+        ? {
+            ...state?.categoryData,
 
-    if (ref === "blogCategoryStories" && params?.category) {
+            [params?.category]: {
+              ...state?.categoryData?.[params?.category],
 
-        return ({
+              [params?.page || 1]: data
+            }
+          }
+        : {
+            ...state?.categoryData,
 
-            categoryData: state?.categoryData?.[params?.category] ?
+            [params?.category]: { [params?.page || 1]: data }
+          },
 
-                ({
+      loader: false
+    };
+  }
 
-                    ...state?.categoryData,
-
-                    [params?.category]: {
-
-                        ...state?.categoryData?.[params?.category],
-
-                        [params?.page || 1]: data
-
-                    }
-
-
-                }) :
-
-                {
-                    ...state?.categoryData,
-
-                    [params?.category]: { [params?.page || 1]: data }
-
-                },
-
-                loader: false
-
-        });
-
-    }
-
-    return ({ data });
-
-}
+  return { data };
+};
 
 const blogReducer = (state = initialState, action: { [key: string]: any }) => {
+  const ref = action?.payload?.ref as blogEntryTypes;
 
-    const ref = action?.payload?.ref as blogEntryTypes;
+  const affectedState = state?.[ref as 'dashboardBlogs' | 'categories' | 'blogCategoryStories'];
 
-    const affectedState = state?.[ref as "dashboardBlogs" | "categories" | "blogCategoryStories"];
+  switch (action.type) {
+    case blogTypes.RETRIEVE_STORIES_START:
+    case blogTypes.RETRIEVE_CATEGORIES_START:
+    case blogTypes.RETRIEVE_CATEGORY_STORIES_START:
+      return {
+        ...state,
 
-    switch (action.type) {
+        [ref]: {
+          ...state[ref],
 
-        case blogTypes.RETRIEVE_STORIES_START:
-        case blogTypes.RETRIEVE_CATEGORIES_START:
-        case blogTypes.RETRIEVE_CATEGORY_STORIES_START:
+          loader:
+            !affectedState?.data?.length ||
+            !shallowCompare(affectedState?.queryParam || {}, action?.payload?.queryParam),
 
-            return {
-    
-                ...state,
-    
-                [ref]: {
+          pageLoader: !shallowCompare(affectedState?.queryParam || {}, action?.payload?.queryParam),
 
-                    ...state[ref],
+          queryParam: action?.payload?.queryParam
+        }
+      };
 
-                    loader: !affectedState?.data?.length || !shallowCompare(affectedState?.queryParam || {}, action?.payload?.queryParam),
+    case blogTypes.RETRIEVE_STORIES_SUCCESS:
+    case blogTypes.RETRIEVE_CATEGORIES_SUCCESS:
+    case blogTypes.RETRIEVE_CATEGORY_STORIES_SUCCESS:
+      return {
+        ...state,
+        [ref]: {
+          ...state[ref],
+          loader: false,
+          pageLoader: false,
 
-                    pageLoader: !shallowCompare(affectedState?.queryParam || {}, action?.payload?.queryParam),
+          ...dataToStore(state?.[ref] as any, action.payload.items, ref, affectedState?.queryParam),
 
-                    queryParam: action?.payload?.queryParam
+          ...(ref === 'categories'
+            ? {
+                pairs: createCategoryPairs(action?.payload?.items),
+                slugPairs: createCategoryPairs(action?.payload?.items, true)
+              }
+            : {})
+        }
+      };
 
-                }
-            };
+    case blogTypes.RETRIEVE_STORIES_FAILURE:
+    case blogTypes.RETRIEVE_CATEGORIES_FAILURE:
+      return {
+        ...state,
+        [ref]: {
+          ...state[ref],
+          loader: false,
+          error: true
+        }
+      };
 
-        case blogTypes.RETRIEVE_STORIES_SUCCESS:
-        case blogTypes.RETRIEVE_CATEGORIES_SUCCESS:
-        case blogTypes.RETRIEVE_CATEGORY_STORIES_SUCCESS:
+    case blogTypes.RETRIEVE_STORY_SUCCESS:
+      return {
+        ...state,
 
-            return {
-                ...state,
-                [ref]: {
-                    ...state[ref],
-                    loader: false,
-                    pageLoader: false,
+        [ref]: {
+          ...(state?.[ref] || {}),
 
-                    ...(dataToStore(state?.[ref] as any, action.payload.items, ref, affectedState?.queryParam)),
+          [action?.payload?.items?.[0]?.slug]: action?.payload?.items?.[0]
+        }
+      };
 
-                    ...(ref === "categories" ? {
-                        pairs: createCategoryPairs(action?.payload?.items),
-                        slugPairs: createCategoryPairs(action?.payload?.items, true)
-                    } : {}),
-
-                }
-            };
-
-        case blogTypes.RETRIEVE_STORIES_FAILURE:
-        case blogTypes.RETRIEVE_CATEGORIES_FAILURE:
-
-            return {
-                ...state,
-                [ref]: {
-                    ...state[ref],
-                    loader: false,
-                    error: true
-                }
-            };
-
-        case blogTypes.RETRIEVE_STORY_SUCCESS:
-
-            return {
-
-                ...state,
-
-                [ref]: {
-
-                    ...(state?.[ref] || {}),
-
-                    [action?.payload?.items?.[0]?.slug]: action?.payload?.items?.[0]
-
-
-                }
-
-            }
-
-        default:
-            return state;
-
-    }
-
+    default:
+      return state;
+  }
 };
 
 export default blogReducer;
