d6fc72e7de76935d7b01976080a40e2d6b9ebd55
Theo Ojochegbe <symplytheo@gmail.com>
2023-10-25 17:53:51 +0100

    feat: add video section

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -8,7 +8,7 @@ import BlogCardGrid from 'common/Blog/BlogCardGrid';
 import { BlogCategories } from 'common/Blog/BaseBlog/BlogCategories';
 import { useRouter } from 'next/router';
 
-const Blog: React.FC<Props> = ({ isMobile, deviceWidth, posts, musics }) => {
+const Blog: React.FC<Props> = ({ isMobile, deviceWidth, posts, musics, videos }) => {
   const [topReads, setTopReads] = useState<blogItemType[]>([]);
 
   useEffect(() => {
@@ -41,7 +41,13 @@ const Blog: React.FC<Props> = ({ isMobile, deviceWidth, posts, musics }) => {
             actionText="Show More Music"
             action={() => router.push('/blog/music')}
           />
-          {/* <BlogCardGrid items={[...Array(6)]} title="Videos" variant="videos" actionText="Show older videos" /> */}
+          <BlogCardGrid
+            items={videos}
+            title="Videos"
+            variant="videos"
+            actionText="Show older videos"
+            action={() => window.open('https://www.youtube.com/@WeTalkSound/videos', '_blank')}
+          />
           <BlogCardGrid items={posts.slice(6, 14)} title="Features" showAction={false} />
         </div>
       </ComponentHolder>
@@ -56,19 +62,28 @@ export async function getServerSideProps() {
   // get music category id
   let musicCategoryID: any = await fetch(`https://blog-admin.wetalksound.co/wp-json/wp/v2/categories?slug=music`);
   musicCategoryID = await musicCategoryID.json();
-  musicCategoryID = musicCategoryID[0].id
+  musicCategoryID = musicCategoryID[0].id;
 
-  
   const baseUrl =
     'https://blog-admin.wetalksound.co/wp-json/wp/v2/posts?_embed=1&_fields=title,slug,categories,date,_links,yoast_head_json.description';
 
-  let [posts, musics] = await Promise.all([
+  const youtubeUrl =
+    'https://www.googleapis.com/youtube/v3/search?key=' +
+    process.env.NEXT_PUBLIC_YOUTUBE_API_KEY +
+    '&channelId=' +
+    process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID +
+    '&part=snippet,id&order=date&maxResults=6';
+
+  let [posts, musics, videos] = await Promise.all([
     fetch(`${baseUrl}&per_page=15`).then(res => res.json()),
-    fetch(`${baseUrl}&categories=${musicCategoryID}`).then(res => res.json())
+    fetch(`${baseUrl}&categories=${musicCategoryID}`).then(res => res.json()),
+    fetch(youtubeUrl)
+      .then(res => res.json())
+      .then(data => data.items)
   ]);
 
   return {
-    props: { posts, musics }
+    props: { posts, musics, videos }
   };
 }
 
@@ -79,4 +94,5 @@ interface Props {
   deviceWidth: number;
   posts: blogItemType[];
   musics: blogItemType[];
+  videos: any[];
 }
