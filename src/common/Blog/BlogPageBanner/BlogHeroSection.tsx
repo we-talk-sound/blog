0c8ece9cb418c465d0335aeceda25e3eb74ee884
AbdussamadYisau <sammieyisau@gmail.com>
2024-04-29 11:08:07 +0100

    Fixed story/blog title not showing up

--- src/common/Blog/BlogPageBanner/BlogHeroSection.tsx
+++ src/common/Blog/BlogPageBanner/BlogHeroSection.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import * as He from 'he';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { transformStory } from 'utils';
 import { blogCategoryItemType, blogItemType } from 'types';
 
-const BlogHeroSection = ({ story, text, items = [], categoryDescription, allCategories = [] }: Props) => {
+const BlogHeroSection = ({
+  story,
+  text,
+  items = [],
+  //  categoryDescription,
+  allCategories = []
+}: Props) => {
   // transform each item
   const transItems = items.map(item => transformStory(item || {}, allCategories) || item);
 
@@ -12,26 +18,27 @@ const BlogHeroSection = ({ story, text, items = [], categoryDescription, allCate
     <section className="page-blog-hero">
       {/* multiple */}
       {transItems?.length ? (
-        <div className="page-blog-hero-multiple">
-          {categoryDescription && <p className="category-description">{categoryDescription}</p>}
-          <div className="grid">
-            {transItems.map((item, idx) => (
-              <div key={idx} className={`grid-item ${idx === 1 || idx === 4 ? 'span-2' : ''}`}>
-                <Link href={`/blog/${item?.category?.toLowerCase() || 'all'}/${item?.item?.slug}`}>
-                  <img src={item?.image} />
-                  <div className="grid-item-content">
-                    <div>
-                      <span className="category">{item?.category || 'All'}</span>
-                      <span>{item?.date}</span>
-                    </div>
-                    <div className="author">By {item?.author}</div>
-                    <h4 className="title">{He.unescape(item?.title || '')}</h4>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
+        // <div className="page-blog-hero-multiple">
+        //   {categoryDescription && <p className="category-description">{categoryDescription}</p>}
+        //   <div className="grid">
+        //     {transItems.map((item, idx) => (
+        //       <div key={idx} className={`grid-item ${idx === 1 || idx === 4 ? 'span-2' : ''}`}>
+        //         <Link href={`/blog/${item?.category?.toLowerCase() || 'all'}/${item?.item?.slug}`}>
+        //           <img src={item?.image} />
+        //           <div className="grid-item-content">
+        //             <div>
+        //               <span className="category">{item?.category || 'All'}</span>
+        //               <span>{item?.date}</span>
+        //             </div>
+        //             <div className="author">By {item?.author}</div>
+        //             <h4 className="title">{He.unescape(item?.title || '')}</h4>
+        //           </div>
+        //         </Link>
+        //       </div>
+        //     ))}
+        //   </div>
+        // </div>
+        <></>
       ) : // single
       story ? (
         <div className="page-blog-hero-single">
--- src/common/Blog/BlogPageBanner/index.tsx
+++ src/common/Blog/BlogPageBanner/index.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { ComponentHolder } from 'components';
 import { Header } from 'layout/LandingLayout/header';
 import { BaseBlogHeading } from 'common/Blog/BaseBlog/BaseBlogHeading';
-// import { transformStory } from 'utils/blog';
+import { transformStory } from 'utils/blog';
 import { blogCategoryItemType, blogItemType } from 'types';
-// import BlogHeroSection from './BlogHeroSection';
+import BlogHeroSection from './BlogHeroSection';
 
 export const BlogPageBanner: React.FC<Props> = ({
   isMobile,
   deviceWidth,
-  // story,
+  story,
   category,
-  allCategories = []
-  // categoryDescription,
-  // text,
-  // items
+  allCategories = [],
+  categoryDescription,
+  text,
+  items
 }) => {
-  // const story_ = story ? { ...transformStory(story, allCategories, false) } : undefined;
+  const story_ = story ? { ...transformStory(story, allCategories, false) } : undefined;
 
   return (
     <ComponentHolder className="no-border page-blog" bodyClass="page-zero-content">
       <Header withFrame={true} isMobile={isMobile} deviceWidth={deviceWidth} active={'/blog'} />
 
       <BaseBlogHeading category={String(category)} allCategories={allCategories} />
-      {/* <BlogHeroSection
+      <BlogHeroSection
         story={story_}
         text={text}
         items={items}
         categoryDescription={categoryDescription}
         allCategories={allCategories}
-      /> */}
+      />
     </ComponentHolder>
   );
 };
